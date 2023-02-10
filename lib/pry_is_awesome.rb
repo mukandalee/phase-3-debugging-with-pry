cbc2c852cd9549a2072631fcbe892b5b5c72eccf
mukandalee <leonardmukanda254@gmail.com>
2023-02-10 07:47:22 +0300

    completed lab

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -1,4 +1,5 @@
 require 'pry'
+$name= "bradly sinden"
 
 def prying_into_the_method
 	inside_the_method = "We're inside the method"
@@ -7,6 +8,8 @@ def prying_into_the_method
 	binding.pry
 	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
 	puts this_variable_hasnt_been_interpreted_yet
+	name ="lee dae hoon"
+	puts "#{$name} is a three times feather weight world champ"
 end
 
 prying_into_the_method
